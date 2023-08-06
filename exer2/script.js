70b5e7f0a6d977c4296127c893161e6b9c196e93
felipesinn <felipesinn01@gmail.com>
2023-08-06 19:16:26 -0300

    Numero 2 ok

---
+++ exer2/script.js
@@ -0,0 +1,11 @@
+const  vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let contador = 0;
+
+for(let i =0; i< vetor.length; i++){
+    contador += vetor[i];
+}
+console.log('Números Percorridos', vetor)
+console.log('A soma de todos os elementos', contador)
+// 2. Desenvolva um algoritmo para percorrer um vetor de 20 elementos
+// inteiros e no final mostre a soma de todos os elementos.
+// Criar um vetor com 20 elementos inteiros
